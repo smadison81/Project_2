0928aafc9b095db8bdfb89da554b8ff8adaeb5d2
Washington <wmashingaidze@gmail.com>
2019-10-07 14:31:28 -0500

    added 2 routes for aggregating total revenue and total units sold

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,6 +8,26 @@ module.exports = function(app) {
     });
   });
 
+  // Get total revenue aggregate
+  app.get("/api/revenue", function(req, res){
+    db.Orders.findAll({
+      include: [db.Orders],
+      attributes: [[db.sequelize.literal('SUM (price * order_qty)'), 'revenue']]
+    }).then(function(data){
+      res.json(data)
+    })
+  })
+  
+  // Get total orders aggregate
+  app.get("/api/orders", function(req, res){
+    db.Orders.findAll({
+      //need to confirm order_id key or define it sequelize models
+      attributes: ['Orders_id', [sequelize.fn('count', sequelize.col('Orders_id')), 'count']]
+    }).then(function(order_data){
+      res.json(order_data)
+    })
+  })
+
   // Create a new items
   app.post("/api/items", function(req, res) {
     db.Item.create(req.body).then(function(dbItems) {
