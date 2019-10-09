0b796c099e7f7957b1914a296cccac0df536f8ec
Tahsin Nazmee <tnazmee@yahoo.com>
2019-10-08 21:11:19 -0500

    pulling master

---
+++ public/js/menu.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // Add a function to scroll to the "About Us" div
+    /*
+    $(".scrollToAbout").click(function () {
+      $('html, body').animate({
+        scrollTop: $(".About-Container").offset().top
+      }, 1000);
+    });
+    */
+
+    $(".scrollToAbout2,/").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".About-Container").offset().top
+        }, 0);
+    });
+
+});
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -25,7 +27,9 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+var syncOptions = {
+  force: false
+};
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
@@ -33,15 +37,21 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
+
+console.log("Step 1 🌎")
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+db.sequelize.sync(syncOptions).then(function () {
+    console.log("Step 2")
+    app.listen(PORT, function () {
+      console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+      );
+    });
+  })
+  .catch(function (err) {
+    console.log(err);
   });
-});
 
 module.exports = app;
