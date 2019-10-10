08000b7ca79349a8d1de3649fbfe59c3c17ea6a2
awaheed <awaheed@utexas.edu>
2019-10-10 09:47:36 -0500

    dashboard view template and updated templates to load different style sheets

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -18,6 +18,10 @@ module.exports = function(app) {
     res.render("menu")
   });
 
+  app.get("/manager",function(req,res){
+    res.render("dashboard")
+  })
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
